bcf488c82144d2a086cb495a00df3ed2c0ba77e3
Radha743 <worldofnara777@gmail.com>
2024-02-06 10:58:10 +0530

    Types

--- packages/engine/globals.d.ts
+++ packages/engine/globals.d.ts
@@ -1,1 +1,14 @@
 declare namespace MistGlobal {}
+
+type Values<T> = T[keyof T]
+type Keys<T> = keyof T
+
+interface MistGlobalObject {}
+
+declare var __MIST__: Window['__MIST__']
+
+interface Window {
+  MistEventType: MistEventTypeT
+  __MIST__: MistGlobalObject
+}
+declare var MistEventType: Window['MistEventType']
